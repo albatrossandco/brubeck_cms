fd0eaf25dccaa07d40fe3bc7150d4ff22c5edafc
Justin Myers <justin@justinmyers.net>
2011-08-08 00:39:19 -0400

    Initial migration from Google Maps API v2 to v3 and removal of Mapstraction v1 dependency

--- brubeck/mapping/templatetags/mapping.py
+++ brubeck/mapping/templatetags/mapping.py
@@ -9,13 +9,6 @@ from brubeck.mapping.models import Map, Place
 
 register = template.Library()
 
-@register.simple_tag
-def add_gmap_api():
-    """
-    Adds the site-wide JavaScript code for the Google Maps API.
-    """
-    return mark_safe('<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s&sensor=false" type="text/javascript"></script>' % settings.MAP_API_KEY)
-
 @register.inclusion_tag('mapping/render_map_js.html')
 def render_map_js(id=None, width=300, height=300):
     """
