ed8fdcf1ac31b5f53073f5a9be62f3adc0e7402c
Justin Myers <JMyers@newshour.org>
2012-01-30 15:12:53 -0500

    Fixed #45

--- brubeck/tagging/admin.py
+++ brubeck/tagging/admin.py
@@ -2,5 +2,15 @@ from brubeck.tagging.models import Tag
 from django.contrib import admin
 
 class TagAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'last_updated',)
+        }),
+        ("Don't touch unless you know what you're doing", {
+            'classes': ('collapse',),
+            'fields': ('slug',)
+        }),
+    )
     prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('last_updated',)
 admin.site.register(Tag, TagAdmin)
